fe503183535a1d6b9491e45a479aee942eda8ee1
NotThatEasy <bm.zitter@gmail.com>
2017-06-28 18:06:53 +0300

    new source working, not spread to files

--- CVTest/Source.cpp
+++ CVTest/Source.cpp
@@ -7,7 +7,60 @@
 //std::exit(code); };
 
 
-
+class menu
+{
+public:
+	menu(std::list<std::string> arr, std::function<void()> funcs[menusize])
+		: current(0), _size(arr.size())
+	{
+		unsigned short i = 0;
+		for (; i < menusize; i++)
+			_funcs[i] = funcs[i];
+		i = 0;
+		for (auto&& lnk : arr)
+		{
+			ptrptr[i++] = lnk;
+		}
+	}
+	int run()
+	{
+		do
+			switch ([this]()->int {
+				SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ 0, 0 });
+				for (size_t i = 0; i < current; i++)
+					std::cout << ptrptr[i].c_str() << '\n';
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_GREEN
+					| FOREGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_BLUE);
+				std::cout << ptrptr[current].c_str();
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_GREEN
+					| FOREGROUND_RED | FOREGROUND_INTENSITY);
+				std::cout << '\n';
+				for (size_t i = current + 1; i < _size; i++)
+					std::cout << ptrptr[i].c_str() << '\n';
+				return _getch(); }())
+			{
+			case Enter:
+				_funcs[current]();
+				break;
+			case 72:
+				current = --current < 0 ? _size - 1 : current;
+				break;
+			case 80:
+				current = ++current >= _size ? 0 : current;
+				break;
+			case Escape:
+				menurunning = false;
+				break;
+			}
+				while (menurunning);
+				return 0;
+	}
+protected:
+	bool menurunning;
+	short current, _size;
+	std::string ptrptr[menusize];
+	std::function<void()> _funcs[menusize];
+};
 
 enum tasks {
 	_EXIT,
@@ -46,14 +99,14 @@ public:
 	{
 
 	}
-	std::thread& operator*()
+	std::thread&& operator*()
 	{
 		return std::move(*m_t);
 	}
 	//Callable object, operator() triggers thread running
 	void operator()(std::function<void()>& lambda)
 	{
-		*m_t = std::thread(lambda);
+		m_t = std::unique_ptr<std::thread>(new std::thread(lambda));
 	}
 	void join()
 	{
@@ -71,6 +124,7 @@ public:
 	{
 		if (m_t->joinable())
 			m_t->join();
+		m_t.release();
 	}
 protected:
 	//Smart pointer, storing thread
@@ -83,7 +137,6 @@ public:
 	processing() {}
 	~processing() {}
 
-	virtual void start() = 0;
 	virtual void stop() = 0;
 
 
@@ -109,7 +162,6 @@ class dest : public processing<T>
 public:
 	dest() {}
 	~dest() {}
-	virtual void start() = 0;
 	virtual void stop() = 0;
 	std::function<void(std::string)> m_on_error;
 private:
@@ -130,7 +182,7 @@ public:
 	videoFramer(cv::VideoCapture&& cap, std::mutex& mut) : cap(std::move(cap)), callback(nullptr)
 		, lck{ mut, std::defer_lock }
 	{}
-	videoFramer(cv::VideoCapture cap, std::function<void(cv::Mat)> callback, std::mutex& mut)
+	videoFramer(cv::VideoCapture&& cap, std::function<void(cv::Mat)> callback, std::mutex& mut)
 		: callback(std::make_unique<std::function<void(cv::Mat)>>(callback))
 		, lck{ mut, std::defer_lock }
 		, cap(std::move(cap))
@@ -157,7 +209,7 @@ public:
 
 		cv::Mat frm(cv::Size(width, height), CV_8UC3);
 		cv::Mat cbcked;
-		cv::VideoWriter wrt("outp.avi", -1, FPS, cv::Size(width, height));
+		//cv::VideoWriter wrt("outp.avi", -1, FPS, cv::Size(width, height));
 		cv::namedWindow("Original");
 		if (callback)
 			cv::namedWindow("CallbackEdit");
@@ -166,12 +218,12 @@ public:
 			lError(std::string("Already running"), 5);
 			return;
 		}
-		lck.lock();
+		//lck.lock();
 		while (true)
 		{
-			m_cv.wait(lck, [] {return lst.size() > 0 ? true : false; });
-			if (*lst.begin() != framing)
-				continue;
+			m_cv.wait(lck, [] {return /*lst.size() > 0 ? */true/* : false*/; });
+			/*if (*lst.begin() != framing)
+				continue;*/
 			cap >> frm;
 			if (callback)
 			{
@@ -180,8 +232,10 @@ public:
 				callback->operator()(cbcked);
 			}
 			cv::imshow("Original", frm);
+			if (cv::waitKey(1) == 27)
+				break;
 		}
-		lck.unlock();
+		//lck.unlock();
 	}
 private:
 	cv::VideoCapture cap;
@@ -234,8 +288,12 @@ public:
 		height = static_cast<unsigned short>(cap.get(CV_CAP_PROP_FRAME_HEIGHT));
 		frame = cv::Mat::Mat(cv::Size(width, height), CV_8UC3);
 	}
-	void start() override {}
 	void stop() override {}
+	void operator()()
+	{
+		this->run();
+	}
+protected:
 	void run()
 	{
 		if (!cap.isOpened())
@@ -243,8 +301,7 @@ public:
 			lError(std::string("Blackwhite filter failed to acquire resource"), 11);
 			return;
 		}
-		cv::namedWindow("Original");
-		cv::namedWindow("Filtered");
+		cv::namedWindow("BW");
 		cv::Mat tmp{ cv::Size(width, height), CV_8UC3 };
 		while (true)
 		{
@@ -263,8 +320,9 @@ public:
 			//tmp.copyTo(tmp);
 			//
 			//
-			cv::imshow("Original", frame);
-			cv::imshow("Edited", tmp);
+			cv::imshow("BW", tmp);
+			if (cv::waitKey(1) == 27)
+				break;
 		}
 	}
 protected:
@@ -288,24 +346,28 @@ public:
 		height = static_cast<unsigned short>(cap.get(CV_CAP_PROP_FRAME_HEIGHT));
 		frame = cv::Mat::Mat(cv::Size(width, height), CV_8UC3);
 	}
-	void start() override {}
+	void operator()()
+	{
+		this->run();
+	}
 	void stop() override {}
+protected:
 	void run()
 	{
+		cap.open(0);
 		if (!cap.isOpened())
 		{
-			lError(std::string("Blackwhite filter failed to acquire resource"), 11);
+			lError(std::string("GBlur filter failed to acquire resource"), 11);
 			return;
 		}
-		cv::namedWindow("Original");
-		cv::namedWindow("Filtered");
+		cv::namedWindow("GBlur");
 		cv::Mat tmp{ cv::Size(width, height), CV_8UC3 };
 		while (true)
 		{
 			cap >> frame;
 			if (frame.empty())
 			{
-				lError(std::string("BW => Original frame seems to be empty..."), 12);
+				lError(std::string("GBlur => Original frame seems to be empty..."), 12);
 				return;
 			}
 			tmp = frame.clone();
@@ -314,10 +376,9 @@ public:
 			//
 			//
 			//tmp.copyTo(tmp);
-			//
-			//
-			cv::imshow("Original", frame);
-			cv::imshow("Edited", tmp);
+			cv::imshow("GBlur", tmp);
+			if (cv::waitKey(1) == 27)
+				break;
 		}
 	}
 private:
@@ -338,7 +399,6 @@ public:
 		height = static_cast<unsigned short>(cap.get(CV_CAP_PROP_FRAME_HEIGHT));
 		frame = cv::Mat::Mat(cv::Size(width, height), CV_8UC3);
 	}
-	void start() override {}
 	void stop() override {}
 	void operator()()
 	{
@@ -349,18 +409,17 @@ protected:
 	{
 		if (!cap.isOpened())
 		{
-			lError(std::string("Blackwhite filter failed to acquire resource"), 11);
+			lError(std::string("Canny filter failed to acquire resource"), 11);
 			return;
 		}
-		cv::namedWindow("Original");
-		cv::namedWindow("Filtered");
+		cv::namedWindow("Canny");
 		cv::Mat tmp{ cv::Size(width, height), CV_8UC3 };
 		while (true)
 		{
 			cap >> frame;
 			if (frame.empty())
 			{
-				lError(std::string("BW => Original frame seems to be empty..."), 12);
+				lError(std::string("Canny => Original frame seems to be empty..."), 12);
 				return;
 			}
 			tmp = frame.clone();
@@ -373,8 +432,9 @@ protected:
 			//tmp.copyTo(tmp);
 			//
 			//
-			cv::imshow("Original", frame);
-			cv::imshow("Edited", tmp);
+			cv::imshow("Canny", tmp);
+			if (cv::waitKey(1) == 27)
+				break;
 		}
 	}
 protected:
@@ -392,13 +452,48 @@ int main()
 {
 	std::mutex mut;
 	std::condition_variable cv_;
-	std::unique_ptr<cv::VideoCapture> ptrVC{ std::move(std::make_unique<cv::VideoCapture>(0)) };
-	std::unique_ptr<cv::VideoCapture> ptr{ std::move(std::make_unique<cv::VideoCapture>(0)) };
 	videoFramer framer(cv::VideoCapture(0), mut);
 	_BW bw(mut, cv::VideoCapture(0));
 	gblur blur(mut, cv::VideoCapture(0));
 	detect det(mut, cv::VideoCapture(0));
-	std::thread thr([&framer] {framer.operator()(); });
-	thr.join();
+	std::unique_ptr<threadWrapper> thr;//([&framer] {framer.operator()(); });
+	std::unique_ptr<threadWrapper> bw_;
+	std::unique_ptr<threadWrapper> blur_;
+	std::unique_ptr<threadWrapper> det_;
+	std::function<void()> funcs[]{ 
+		[&framer, &thr] { 
+			thr = std::make_unique<threadWrapper>(
+				[&framer] {
+					framer.operator()(); 
+				}); 
+		}
+	, [&bw, &bw_] {
+			bw_ = std::make_unique<threadWrapper>(
+				[&bw] {
+					bw.operator()(); 
+			}); 
+		}
+	, [&blur, &blur_] { 
+			blur_ = std::make_unique<threadWrapper>(
+				[&blur] {
+					blur.operator()(); 
+				}); 
+		}
+	, [&det, &det_] {
+			det_ = std::make_unique<threadWrapper>(
+				[&det] {
+					det.operator()(); 
+				}); 
+		} 
+	};
+	std::string strs[]{ "Original video","BW video","GBlur video","Canny edge video" };
+	for (auto lnk : funcs)
+		lnk();
+	/*menu my(std::list<std::string>(&strs[0], &strs[4]), funcs);	
+	my.run();*/
+	thr->join();
+	bw_->join();
+	blur_->join();
+	det_->join();
 	return 0;
 }
